023dabf97febcccebf047bdd3299feaf8bbd23b0
ysatyam129 <yadavsatyamsingh078@gmail.com>
2025-08-02 09:33:39 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,35 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Welth",
+  description: "One stop Finance Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with 💗 by RoadsideCoder</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
